6dced3d74705166cfd0e9aa2bcdc8a285340ce58
Zbigniew <zbyhadrian@gmail.com>
2013-04-06 22:12:15 +0200

    zaimplementowana struktura tablicy
    Z grubsza struktura tablicy została zaimplementowana. Jedynie co to
    należy nanieść poprawki które wyjdą w trakcie testów.

--- Projekt_1/tablica.cpp
+++ Projekt_1/tablica.cpp
@@ -3,9 +3,47 @@
 
 tablica::tablica(void)
 {
+	tab=0;
+	ilosc_elementow=0;
+	ilosc_tablic=0;
 }
-
-
 tablica::~tablica(void)
 {
 }
+void tablica::usun_element(int i){
+	int *tab2 = new int[ilosc_elementow-1];
+	for(int a=0;a<=ilosc_elementow;a++){
+		if(a!=i){
+			tab2[a]=tab[a];
+		}
+	}
+	tab=tab2;
+	ilosc_elementow--;
+};
+void tablica::dodaj_element(int i){
+	int *tab_pomocnicza = new int[ilosc_elementow+1];
+	for(int a=0;a<=ilosc_elementow;a++){
+		tab_pomocnicza[a]=tab[a];}
+	tab_pomocnicza[ilosc_elementow+1]=i;
+	tab=tab_pomocnicza;
+	ilosc_elementow++;
+};
+int tablica::wyszukaj_element(int i){
+	for(int a=0;a<=ilosc_elementow;a++){
+		if(tab[a]=i){return a;}
+		else return -1;
+	}
+};
+void tablica::skasuj_tablice(){
+	tab=0;
+	ilosc_elementow=0;
+};
+void tablica::nowa_tablica(int ilosc){
+	if(tab!=0){
+		ilosc_elementow=ilosc;
+		tab=new int[ilosc];
+		ilosc_tablic++;
+	}
+};
+int tablica::podaj_ilosc_elementow(){return ilosc_elementow;};
+int tablica::podaj_ilosc_tablic(){return ilosc_tablic;};
--- Projekt_1/tablica.h
+++ Projekt_1/tablica.h
@@ -1,13 +1,20 @@
 #ifndef TABLICA_HEADER
 #define TABLICA_HEADER
-class tablica
-{
+class tablica{
+private:
+	int *tab, ilosc_tablic, ilosc_elementow;	 //wszystkie potrzebne statystyki
 public:
-	int *tab;
-	void dodaj_element();
-	void usun_element();
-	void nowa_tablica();
-	void skasuj_tablice();
+	// metody potrzebne do pomiarów
+	void dodaj_element(int i);					 
+	void usun_element(int i);	
+	int wyszukaj_element(int i);
+	// metody pozwalajace pracowac na jednym obiekcie (mniej kodu)
+	void nowa_tablica(int ilosc);				 
+	void skasuj_tablice();					
+	// metody do statystyk
+	int podaj_ilosc_elementow();				
+	int podaj_ilosc_tablic();
+	// konstruktory itd
 	tablica(void);
 	~tablica(void);
 };
